5a1977d9f91648a1bdd5927774d4b92fc2d0478b
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 04:49:00 +0500

    Lecture Completed

--- variable.py
+++ variable.py
@@ -8,14 +8,42 @@ email = "Bro123@fake.com"
 # print(f"You Like! {food}") 
 # print(f"Thank you! {email}") 
 
-#Integer 
+#Integer -> Whole Numbers
 #Note Numbers should not be in quotes.
 age = 35
 quantity = 3
 
 
-print(f"You are {age} years Old.")
-print(f"You are Buying {quantity} Apples.")
+# print(f"You are {age} years Old.")
+# print(f"You are Buying {quantity} Apples.")
 
+#Floats -> Number with Points
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+
+
+print(f"Your GPA is {gpa}.")
+print(f"The Price is {price}.")
+print(f"You ran {distance}.")
+
+#Booleans ->True Or False
+
+is_Students = True
+for_sale = True
+
+print(f"Are you a Student?: {is_Students} ")
+
+if is_Students:
+    print("You are a Student.")
+else:
+     print("You are Not a Student.")
+    
+if for_sale:
+     print("Yes For Sale.")
+else:      
+     print("Not For Sale.")
 
 
