e25cdc30b873edd0cbda2418905c49154b4d00ae
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 04:56:13 +0500

    Typecasting

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+#TYPECASTING -> ONE DATA TYPE TO ANOTHER DATA TYPE
+
+name = "Sherry"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(name)) #Print Type Of Data Type
+
+gpa = int(gpa)
+print(gpa) 
+
+age = float(age)
+print(age) 
+
+age = str(age)
+print(age)  
+
+name = bool(name)
+print(name) 
+
+ 
