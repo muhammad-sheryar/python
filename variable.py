49e1dcbd8ea3dd8e4e2679bbbdfd909787c3d970
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 04:36:57 +0500

    Variables

---
+++ variable.py
@@ -0,0 +1,13 @@
+
+#Strings -> Series of Characters
+first_name = "Bro"
+food = "Pizza"
+email = "Bro123@fake.com"
+
+print(f"Hello! {first_name}") 
+print(f"You Like! {food}") 
+print(f"Thank you! {email}") 
+
+#Integer 
+
+age = 35
