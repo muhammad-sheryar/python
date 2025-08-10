050bcc930fe76e1e9d2909c1a867486d6380a3a4
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 04:40:30 +0500

    Integers

--- variable.py
+++ variable.py
@@ -4,10 +4,18 @@ first_name = "Bro"
 food = "Pizza"
 email = "Bro123@fake.com"
 
-print(f"Hello! {first_name}") 
-print(f"You Like! {food}") 
-print(f"Thank you! {email}") 
+# print(f"Hello! {first_name}") 
+# print(f"You Like! {food}") 
+# print(f"Thank you! {email}") 
 
 #Integer 
-
+#Note Numbers should not be in quotes.
 age = 35
+quantity = 3
+
+
+print(f"You are {age} years Old.")
+print(f"You are Buying {quantity} Apples.")
+
+
+
