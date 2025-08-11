2341dd12aded34c0711b41c2e53334e019665303
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 16:15:49 +0500

    Exercise

--- string_methods.py
+++ string_methods.py
@@ -13,6 +13,20 @@
 # result = name.isalpha()
 # result = phone_number.count("-")
 # result = phone_number.replace("-"," ")
-print(help(str))
+# print(help(str))
 
 # print(result)
+
+#Exercise
+
+username = input("Enter a Name: ")
+
+
+if len(username) > 12:
+    print("Can't be more than 12 Characters!")
+elif not username.find(" ") == -1:
+    print("Can't Contain Spaces!")
+elif not username.isalpha():
+    print("Can't Use Digits!")
+else: 
+    print(f"Welcome {username}")
