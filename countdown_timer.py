1c33620a66c5052654f65635d6ce93c23cee50a3
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 18:54:34 +0500

    Count-Down Timer

---
+++ countdown_timer.py
@@ -0,0 +1,16 @@
+#Python Count-Down Timer 
+
+import time
+
+my_time = int(input("Enter the time in Seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60 #can't go above % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+
+
+print("Time's Up!")
