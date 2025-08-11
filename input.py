818302ce6bfccfc0461296f5169c92d99d9fe193
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 14:09:39 +0500

    Exercise

--- input.py
+++ input.py
@@ -20,12 +20,26 @@
 
 #Exercise
 
-item =  input("What item would you like to Buy?: ")
-price = float(input("What is Price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
+# item =  input("What item would you like to Buy?: ")
+# price = float(input("What is Price?: "))
+# quantity = int(input("How many would you like?: "))
+# total = price * quantity
 
-print(f"You have Bought{item} * {quantity}")
-print(f"Your Total is: ${total}")
+# print(f"You have Bought{item} * {quantity}")
+# print(f"Your Total is: ${total}")
+
+#Madlibs Game
+#Word Game Where you create story
+#By filling in the blanks with random words
+
+adjective1 = input("ENter an Adjective: ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an Adjective: ")
+verb1 = input("Enter a verb (ending with ing):")
+adjective3 = input("Enter an Adjective: ")
 
 
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In an exhibit,  I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+(f"I was {adjective3}")
