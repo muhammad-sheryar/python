119d2c3878030151623d41fa8b298ef1df2836ec
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 16:07:47 +0500

    String Methods

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+#Python String Methods
+
+# name = input("Enter Your Full Name: ")
+# phone_number = input("Enter Phone Number: ")
+
+# result = len(name)
+# result = name.find("Sher")
+# result = name.rfind("o")
+# result = name.capitalize()
+# result = name.upper()
+# result = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_number.count("-")
+# result = phone_number.replace("-"," ")
+print(help(str))
+
+# print(result)
