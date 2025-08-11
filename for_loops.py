60f044f2e97822aa6cd0c20cf527f1a3987a4bd4
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 18:36:12 +0500

    For Loops

---
+++ for_loops.py
@@ -0,0 +1,16 @@
+#Python for loops -> Fixed Number of Times
+
+# for i in reversed(range(1,11,2)):
+#     print(i)
+    
+# print("Happy New Year!")
+    
+# credit_no = "1234-5678-9012-3456"
+
+# for x in credit_no:
+#     print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        continue
+    print(x, end=" ")
