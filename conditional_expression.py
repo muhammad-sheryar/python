903a5c478eb88eb8579003084a7d43b35d392764
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 15:57:00 +0500

    Conditional Expression

---
+++ conditional_expression.py
@@ -0,0 +1,32 @@
+#Python Conditional Expression -> One Line Shortcut For if_else Statement (X if Conditio else Y)
+
+
+num = 5
+a = 5
+b = 6
+age = 25
+temperature = 30
+user_role = "guest"
+
+
+
+# print("Positive" if num > 0 else "Negative")
+
+# result = "Even" if num % 2 == 0 else "Odd"
+# print(result)
+
+
+# max_num = a if a > b else b
+# print(max_num)
+
+# min_num = a if a < b else b
+# print(min_num)
+
+# status = "Adult!" if age > 18 else "Child!"
+# print(status)
+
+# weather = "Hot!" if age > 25 else "Normal!"
+# print(weather)
+
+user = "Full Access!" if user_role == "admin" else "Access Denied!"
+print(user)
