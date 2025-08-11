64ffaeecb1945d77db01eadf1d8cc26a9e0e1d62
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 05:10:23 +0500

    Input

---
+++ input.py
@@ -0,0 +1,31 @@
+#Input -> Take Input From User
+#Can't operate differenct data types
+
+# name = input("What is your Name?: ")
+# age = int(input("How old are you?: "))
+
+# age += 1
+
+# print(f"Hello {name}!")
+# print("Happy Birthday!")
+# print(f"You are {age} years old!")
+
+#Excercise
+
+# length = int(input("Enter the length: "))
+# width = int(input("Enter the width: "))
+# area = length * width
+
+# print(f"Area is: {area}cm²") #alt + 0178 -> super script
+
+#Exercise
+
+item =  input("What item would you like to Buy?: ")
+price = float(input("What is Price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have Bought{item} * {quantity}")
+print(f"Your Total is: ${total}")
+
+
