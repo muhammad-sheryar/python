2ad8a5a3127f152df0b73956c8cbb1e994c2817f
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 14:38:52 +0500

    Arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,66 @@
+#Arithmetic -> +,-,*,/
+
+friends = 5
+friends = friends + 1
+friends += 1
+friends = friends - 2
+friends -= 2
+friends = friends * 2
+friends *= 2
+friends = friends / 2
+friends /= 2
+friends = friends ** 2
+friends **= 2
+friends = friends % 2
+friends %= 2
+
+# print(friends)
+
+x = 3.14
+y = -4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(x,y,z)
+# result = min(x,y,z)
+
+# print(result)
+
+import math #Library
+
+# print (math.pi)
+# print (math.e)
+
+x = 9.5
+# result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
+
+# print(result)
+
+#Exercise
+
+# circumference = 2 * math.pi * radius
+
+# print(f"The Circumference is: {round(circumference,2)}cm")
+
+#Exercise 
+
+# radius = int(input("Enter the Radius of a Circle: "))
+
+# area = math.pi * pow(radius,2)
+
+# print(f"The area of Circle is: {round(area, 2)}cm²")
+
+#Exercise
+
+# a = float(input("Enter Side a: "))
+# b = float(input("Enter Side b: "))
+
+# c = math.sqrt(pow(a,2) + pow(b,2))
+
+# print(f"Side c: {c}")
+
+
