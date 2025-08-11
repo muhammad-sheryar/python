f2a78c3a2b72a9442f63e39958ca46cfae349a7a
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 17:44:13 +0500

    Indexing

---
+++ indexing.py
@@ -0,0 +1,16 @@
+#Python Indexing
+
+credit_no = "1234-5678-9012-3456"
+
+# print(credit_no[2])
+# print(credit_no[:4])
+# print(credit_no[5:9])
+# print(credit_no[5:])
+# print(credit_no[-4])
+# print(credit_no[::3])
+
+# last_digits = credit_no[-4:] #Count from right but start from left
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# credit_no = credit_no[::-1]
+# print(credit_no)
