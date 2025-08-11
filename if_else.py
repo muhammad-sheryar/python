0d0f2b61a52749f6df1e243a2aa4dd7da4c987e7
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 14:50:10 +0500

    If_Else

---
+++ if_else.py
@@ -0,0 +1,36 @@
+#if_else -> Conditon Based
+
+# age = int(input("Enter Age: "))
+
+# if age >= 100:
+#     print("Too Old!")
+# if age >= 18:
+#     print("Eligible!")
+# elif age < 0:
+#     print("Not Born yet!")
+# else:
+#     print("Not Eligible!")    
+
+# food = input("Would you like food? (Y/N): ")
+
+# if food == "Y":
+#     print("Welcome To Restaurant!")
+# else:
+#     print("No Food!")    
+
+# name = input("Enter Your Name: ")
+
+# if name == "":
+#     print("Not type your name.")
+# else:
+#     print(f"Hello {name}!")
+
+# for_sale = True
+
+# if for_sale == True:
+#     print("This item is for sale!")
+# else:
+#     print("This item is Not for sale!")
+
+
+    
