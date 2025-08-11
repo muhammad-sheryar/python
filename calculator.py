d313903b1ad0adaa6aa837938a7cadae74e15c35
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-11 14:58:52 +0500

    Calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#python calculator
+
+# operator = input("Enter an Operator (+ - * /): ")
+# num1 = float(input("Enter 1st Number: "))
+# num2 = float(input("Enter 2nd Number: "))
+
+# if operator == "+":
+#     print(f"Addition is: {num1 + num2}")
+# elif operator == "-":
+#     print(f"Substraction is: {num1 - num2}")
+# elif operator == "*":
+#     print(f"Multiplication is: {num1 * num2}")
+# elif operator == "/":
+#     print(f"Division is: {num1 / num2}")
+# else:
+#     print(f"{operator} is Invalid Operator!")
