e782b2154c98a534b2176bb53444a9f0fae6e1a5
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-12 07:07:49 +0500

    Rock Scissor Paper

---
+++ rock_paper_scissor.py
@@ -0,0 +1,33 @@
+#Python Rock Paper Scissors
+
+
+import random
+options = ("rock", "paper", "scissor")
+running = True
+
+while running:
+    player = None
+    computer = random.choice(options)
+    while player not in options:
+        player = input("Enter a Choice (rock, paper, scissors): ")
+        
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("Tie!")
+    elif player == "rock" and computer == "scissor":
+        print("You Win!")
+    elif player == "paper" and computer == "rock":
+        print("You Win!")
+    elif player == "scissor" and computer == "paper":
+        print("You Win!")
+    else:
+        print("You Lose!")
+        
+    play_Again =input("Play Again? (y/n): ").lower()
+    if not play_Again == "y":
+        break
+    
+print("Thanks For Playing")
