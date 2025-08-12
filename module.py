b122f9dad3b3222cd058119c921585fb593877af
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-13 00:58:27 +0500

    Modules

---
+++ module.py
@@ -0,0 +1,26 @@
+#Python Modules -> A file containing Code you want to include into your program e.g module_new
+
+
+# help("modules")
+# help(help("math"))
+
+# import math
+# import math as m
+# from math import e
+
+# a,b,c,d = 1,2,3,4
+
+# print(math.e ** a)
+# print(math.e ** b)
+# print(math.e ** c)
+# print(math.e ** d)
+
+import module_new
+
+# result = module_new.pi
+# result = module_new.square(3)
+# result = module_new.cube(3)
+# result = module_new.circumference(3)
+result = module_new.area(3)
+
+print(result)
