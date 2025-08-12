201cd064531eef892d657e1560d33169ba3ba1ed
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-13 00:10:02 +0500

    Membership Operator

---
+++ membership_opertaors.py
@@ -0,0 +1,41 @@
+#Python Membership Operator -> in/not in
+
+
+# word = "apple"
+
+# letter = input("Guess a letter in a Secret Word: ")
+
+# if letter not in word:
+#     print(f"There is a no letter {letter}!")
+# else:
+#     print(f"There is a letter {letter}.")
+
+
+# students = {"Sherry", "Huzaifa", "Kiswa"}
+
+# student = input("Enter a Student Name: ")
+
+# if student in students:
+#     print(f"{student} is a Student.")
+# else:
+#     print(f"{student} is not a Student.")
+
+
+# grades = {"Sherry" : "A", "Kiswa" : "B", "Abeer" : "C", "Kabeer" : "D"} 
+
+
+# student = input("Enter The Name of Studen: ")
+
+# if student in grades:
+#     print(f"{student}'s Grade is {grades[student]}")     
+# else:
+#     print(f"{student} was Not Found!")  
+
+
+# email = "sherry@gmail.com"
+
+# if "@" in email and "." in email:
+#     print("Valid Email!")
+# else:
+#     print("Invalid Email!")
+    
