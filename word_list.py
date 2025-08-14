c952a2ed630314e18fc47925f28e05eb23c92025
muhammad-sheryar <muhammadsheryar081@gmail.com>
2025-08-14 17:50:33 +0500

    Hangman Game

---
+++ word_list.py
@@ -0,0 +1,5 @@
+words = ("python", "java", "kotlin", "javascript", "hangman", "programming",
+    "developer", "function", "variable", "condition", "loop", "string",
+    "integer", "boolean", "dictionary", "tuple", "list", "array",
+    "object", "class", "inheritance", "algorithm", "debug", "compile",
+    "execute", "syntax", "exception", "module", "package", "library")
